b1739b2b9572953fbdf166ecdf20656879f707b0
peixiongau <70298430+peixiongau@users.noreply.github.com>
2021-04-02 21:02:32 +0800

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -95,7 +95,7 @@ def _check_grid(grid):
             
             if x == '1024':
                 msg = "error: invalid grid"
-                return msg + num
+                return msg + grid
         
         if accum in valid_nums:
             grid_parsed.append(accum)
