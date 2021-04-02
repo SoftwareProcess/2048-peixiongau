f6f9a7984ae72b54d50da38537de3a839aa210d3
peixiongau <70298430+peixiongau@users.noreply.github.com>
2021-04-02 19:32:10 +0800

    Red Light |

--- Tiles2048/test/shift_test.py
+++ Tiles2048/test/shift_test.py
@@ -15,34 +15,35 @@ class Test(unittest.TestCase):
         actual = shift._check_parms(parms)
         self.assertEqual(actual, expected)
 
-    def test010_001_validating_parms(self):
+    def test010_002_validating_parms(self):
         parms = {"grid": "000022224444", "score":0, "direction":"abc", "integrity": "00000"}
         expected = {"status": "error: invalid grid"}
         actual = shift._check_parms(parms)
         self.assertEqual(actual, expected)
     
-    def test010_001_validating_parms(self):
+    def test010_003_validating_parms(self):
         parms = {"score":0, "direction":"abc", "integrity": "00000"}
         expected = {"status": "error: missing grid"}
         actual = shift._check_parms(parms)
         self.assertEqual(actual, expected)
-    def test010_001_validating_parms(self):
+    def test010_004_validating_parms(self):
         parms = {"grid": "000022224444", "score":0, "direction":"abc", "integrity": "00000"}
         expected = {"status": "error: bad integrity value"}
         actual = shift._check_parms(parms)
         self.assertEqual(actual, expected)
     
-    def test010_001_validating_parms(self):
+    def test010_005_validating_parms(self):
         parms = {"grid": "000022224444", "score":0, "direction":"abc", "integrity": "00000"}
         expected = {"status": "error: invalid score"}
         actual = shift._check_parms(parms)
         self.assertEqual(actual, expected)
         
-    def test010_001_validating_parms(self):
+    def test010_006_validating_parms(self):
         parms = {"grid": "16161616222244448888", "score":0, "direction":"abc", "integrity": "00000"}
         expected = {"status": "error: no shift possible"}
         actual = shift._check_parms(parms)
         self.assertEqual(actual, expected)
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
