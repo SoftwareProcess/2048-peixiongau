5329569ceaa7eb36c28d89ee4229f6f0aa420112
peixiongau <70298430+peixiongau@users.noreply.github.com>
2021-04-02 21:16:54 +0800

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -41,7 +41,7 @@ def _check_parms(userParms):
         result["status"] = msg
         return result
     grid = userParms["grid"]
-    msg = _check_grid(grid[0])
+    msg = _check_grid(grid['grid'])
     if "passed" not in msg:
         result["status"] = msg
         return result
