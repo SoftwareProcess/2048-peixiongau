12aeb52932a321172bbb86d3ff8ead88cc693eac
peixiongau <70298430+peixiongau@users.noreply.github.com>
2021-04-16 12:29:48 +0800

    Update create.py

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -23,7 +23,7 @@ def _generateGrid():
         if left == 0:
             result += '0'
             continue
-        if random() < 0.2 + i * 0.03:
+        if random() < 0.2 + i * 0.02:
             result += '2'
             left -= 1
         else:
